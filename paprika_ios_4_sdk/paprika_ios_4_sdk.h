d187d5973fe4326c6fc593822c0432c6bb4140f9
doyoung park <do@estmob.com>
2017-01-05 11:21:55 +0900

    Initial Commit

---
+++ paprika_ios_4_sdk/paprika_ios_4_sdk.h
@@ -0,0 +1,19 @@
+//
+//  paprika_ios_4_sdk.h
+//  paprika_ios_4_sdk
+//
+//  Created by 박도영 on 05/01/2017.
+//  Copyright © 2017 do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for paprika_ios_4_sdk.
+FOUNDATION_EXPORT double paprika_ios_4_sdkVersionNumber;
+
+//! Project version string for paprika_ios_4_sdk.
+FOUNDATION_EXPORT const unsigned char paprika_ios_4_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paprika_ios_4_sdk/PublicHeader.h>
+
+
